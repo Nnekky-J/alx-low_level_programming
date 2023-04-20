03954dda5d8f98a4dc50e65e27cb113b3e4626c6
Nnekky-J <nnekaukachukwu93@gmail.com>
2023-04-20 13:28:14 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
   */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora korpar, 2015-10-19\r", 59);
+	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\r", 59);
 	return (1);
 }
