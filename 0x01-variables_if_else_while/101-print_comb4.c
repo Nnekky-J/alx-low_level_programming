8abf62e1d35458d84f1402499701b8a5040dd8c2
Nnekky-J <nnekaukachukwu93@gmail.com>
2023-04-21 14:04:23 -0700

    The success combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *Description: 'print all possible'
+  *Return: Always 0 (Successful)
+  */
+
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+	int k = '0';
+
+	while (i <= '7')
+	{
+		while (j <= '8')
+		{
+			while (k <= '9')
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++;
+			}
+			k = 'o';
+			j++;
+		}
+		j = 'o';
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
