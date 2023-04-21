f8728226f1c3d4ea4d7a65008a13e7ffa7d822eb
Nnekky-J <nnekaukachukwu93@gmail.com>
2023-04-21 10:36:35 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: '9-print_comb.c'
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
