3e35b873e21c8c84a1f00962e5df52c571f15916
Nnekky-J <nnekaukachukwu93@gmail.com>
2023-04-25 07:07:08 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  *print_sign - Prints the sign of a number
+  *
+  *@n: A letter in the ASCII table
+  *
+  *Return: 1 if n is greater than 0 and otherwise return 0
+  *
+  */
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n > 48)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
