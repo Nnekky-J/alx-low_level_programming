6ef13cf28031a2fd6e9cd5c597e1572467924011
Nnekky-J <nnekaukachukwu93@gmail.com>
2023-04-25 06:25:46 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * _isalpha- Shows 1 if the input is a letter and 0 otherwise
+  *
+  * @c: Is the ASCII letter
+  *
+  *Return -  1 if it is a letter and otherwise 0
+  *
+  */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
