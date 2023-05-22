7193359690ea4b1cdfd1901272e5ceaafee53661
Nnekky-J <nnekaukachukwu93@gmail.com>
2023-05-22 08:48:29 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  *init_dog - Name of the struct
+  *
+  *@d: The first member
+  *
+  *@name: The second member
+  *
+  *@age: The third member
+  *
+  *owner: The fourth member
+  *
+  *Description: The name of struct dog
+  *
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
